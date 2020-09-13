a48be37816901fb8abf361ef121c9d1047fa5755
PMacStew <23pmcclure@athenian.org>
2020-09-13 00:31:26 +0000

    e

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,32 +4,54 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double guess = 3;
+        double betterGuess = 0;
+        double temporary = 0;
+        while (Math.abs(betterGuess - guess) < 0.0000001) {
+            betterGuess = (guess + (x / guess)) / 2;
+            temporary = guess;
+            guess = betterGuess;
+            betterGuess = temporary;
+        }
+        return guess;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int last = 0;
+        for (int i = 0; i < x; i++) {
+            if (x == 0) 
+                return 1; 
+            if (i == 0)
+                last = last + 1;
+            else
+                last = last * (i + 1);
+        }
+        return last;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int count = 0;
+        double e = 0;
+        while (Math.abs(e - Math.E) <= 0.00000001) {
+            e = e + (1 / factorial(count));
+            count++;
+        }
+        return e;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
+        System.out.print("Enter a number: ");
+        double input = scan.nextDouble();
+        System.out.println("The square root of your number is approxamitely " + babylonian(input) + ".");
+        int input2 = scan.nextInt();
+        System.out.println(factorial(input2));
+        System.out.println("The aproxamite value of E is " + calculateE() + ".");
 
         scan.close();
     }
